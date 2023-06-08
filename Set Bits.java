129774a60cec28cf3adbbb2cd9c34845ab0bc474
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-08 14:01:41 +0530

    Create Set Bits.java

---
+++ Set Bits.java
@@ -0,0 +1,25 @@
+import java.util.* ;
+
+import java.io.*; 
+
+public class Solution {
+
+    public static int countSetBits(int n) {
+
+        // Write your code here.
+
+           int count = 0;
+
+    while (n > 0) {
+
+      n &= n - 1;
+
+      count++;
+
+    }
+
+    return count;
+
+    }
+
+}
