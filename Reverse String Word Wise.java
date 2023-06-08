e27e1aaaa9fc286d522c99090dd6df2d46469bc1
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-08 19:58:32 +0530

    Create Reverse String Word Wise.java

---
+++ Reverse String Word Wise.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+ 
+
+class Solution {
+
+ 
+
+static String reverseStringWordWise(String input) {
+
+// Write your code here
+
+String[] res = input.split(" ");
+
+String rev = "";
+
+for(int i=(res.length)-1; i>=0; i--){
+
+rev+=res[i];
+
+rev+=" ";
+
+}
+
+return rev;
+
+}
+
+ 
+
+public static void main(String args[]) {
+
+Scanner sc = new Scanner(System.in);
+
+String input = sc.nextLine();
+
+String ans = reverseStringWordWise(input);
+
+System.out.println(ans);
+
+}
+
+}
