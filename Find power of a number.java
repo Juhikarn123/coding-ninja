cf904fe2b0f4d1900b6ab9fb7de15cd5f657c755
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-08 13:58:37 +0530

    Create Find power of a number.java

---
+++ Find power of a number.java
@@ -0,0 +1,25 @@
+import java.util.* ;
+
+import java.io.*; 
+
+class Solution {
+
+    
+
+    public static void main(String args[]) {
+
+        
+
+        // Write code here
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        int n = sc.nextInt();
+
+        System.out.print((int)Math.pow(x,n));
+
+    }
+
+}
