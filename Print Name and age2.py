4e91dc34e2a8b50ca9f7320f18d59179f319f993
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-08 13:42:40 +0530

    Create Print Name and age2.py

---
+++ Print Name and age2.py
@@ -0,0 +1,7 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+n=input()
+age=int(input())
+print("The name of the person is {} and the age is {}.".format(n,age))
