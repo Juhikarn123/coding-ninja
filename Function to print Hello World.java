3027dd5c11acc125525fe18b711ea9572b98d2c4
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-08 14:00:40 +0530

    Create Function to print Hello World.java

---
+++ Function to print Hello World.java
@@ -0,0 +1,13 @@
+class Solution {
+	
+	// Write your print function here
+	public void print(){
+		System.out.println("Hello World");
+	}
+ 
+	
+	public static void main(String args[]) {
+		Solution obj = new Solution();
+		obj.print();
+	}
+}
