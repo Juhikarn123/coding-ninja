da40d61e0f1201ac88e7081259862339350b6cbc
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-08 20:00:26 +0530

    Create Area of a Rectangle oops.java

---
+++ Area of a Rectangle oops.java
@@ -0,0 +1,19 @@
+import java.util.* ;
+
+import java.io.*; 
+
+public class Rectangle { 
+
+    // Write your code here.
+
+    public int length;
+
+    public int breadth;
+
+    public int getArea(){
+
+        return this.length*this.breadth;
+
+    }
+
+}
